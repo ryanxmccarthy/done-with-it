9e292ec24b50c7c0122db3ad9e0677ac7f54ce35
Ryan XVX <ryanxmccarthy@gmail.com>
2020-09-02 21:59:59 -0700

    setting up main view

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: "white" }}>Inside me there are two wolves.</Text>
+      <Text style={{ color: "white" }}>One is bad at writing code.</Text>
+      <Text style={{ color: "white" }}>
+        The other one is a wolf, so he also can't code.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    color: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
