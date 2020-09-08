a471a8845b12c285b37ec1b877e43578c2a732c7
Ryan XVX <ryanxmccarthy@gmail.com>
2020-09-08 15:37:44 -0700

    experimenting with touchables

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
-      <Image source={require("./assets/icon.png")} />
+      <Text>
+        Someday this will be a 1RM calculator. Until then, enjoy this image:
+      </Text>
+      <TouchableOpacity onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
