15ee47572172a84cbe958767fd13f41507ca6c39
Ryan XVX <ryanxmccarthy@gmail.com>
2020-09-09 16:58:29 -0700

    working on main views

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Button, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="Click me" onPress={() => alert("button tapped")} />
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 150, height: 70 }}
+      ></View>
     </SafeAreaView>
   );
 }
