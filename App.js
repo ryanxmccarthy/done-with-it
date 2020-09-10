52e88b3a6a4c238542a39916ab69156a3bc2019a
Ryan XVX <ryanxmccarthy@gmail.com>
2020-09-09 17:06:24 -0700

    working on responsivity between portrait/landscape

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from "react";
 import {
+  Dimensions,
   StyleSheet,
   SafeAreaView,
   StatusBar,
   Platform,
   View,
 } from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
+
   return (
     <SafeAreaView style={styles.container}>
       <View
-        style={{ backgroundColor: "dodgerblue", width: 150, height: 70 }}
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: landscape ? "100%" : "30%",
+        }}
       ></View>
     </SafeAreaView>
   );
@@ -21,7 +32,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
