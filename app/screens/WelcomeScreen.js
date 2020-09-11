cd24b4381b1253984b366466e8370858d7e41907
Ryan XVX <ryanxmccarthy@gmail.com>
2020-09-10 17:00:44 -0700

    home page outline done

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -7,10 +7,16 @@ function WelcomeScreen(props) {
       source={require("../assets/background.jpg")}
       style={styles.background}
     >
-      <Image source={require("../assets/logo.png")} style={styles.logo} />
-      <Text style={{ color: "white" }}>One Rep Max Calculator</Text>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/logo.png")} style={styles.logo} />
+        <Text style={{ color: "white" }}>One Rep Max Calculator</Text>
+      </View>
+      <View style={styles.loginButton}>
+        <Text style={styles.buttonText}>Login</Text>
+      </View>
+      <View style={styles.registerButton}>
+        <Text style={styles.buttonText}>Register</Text>
+      </View>
     </ImageBackground>
   );
 }
@@ -21,20 +27,30 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-end",
   },
+  buttonText: {
+    color: "white",
+  },
   loginButton: {
+    alignItems: "center",
     backgroundColor: "#9c0c0cbb",
     height: 70,
+    justifyContent: "center",
     width: "100%",
   },
   logo: {
     height: 200,
-    position: "absolute",
-    top: 70,
     width: 200,
   },
+  logoContainer: {
+    alignItems: "center",
+    position: "absolute",
+    top: 50,
+  },
   registerButton: {
+    alignItems: "center",
     backgroundColor: "#540505bb",
     height: 70,
+    justifyContent: "center",
     width: "100%",
   },
 });
